a13ac6f6b5c034fa0d3ce4558214d56e26cb6f84
sirmcclay <mcclay.design@gmail.com>
2021-02-18 12:58:51 -0300

    change count for countDocuments

--- test/controllers/drivers_controller_test.js
+++ test/controllers/drivers_controller_test.js
@@ -7,12 +7,12 @@ const Driver = mongoose.model('driver');
 
 describe('Drivers controller', () => {
 	it('Post to /api/drivers creates a new driver', (done) => {
-		Driver.count().then((count) => {
+		Driver.countDocuments().then((count) => {
 			request(app)
 				.post('/api/drivers')
 				.send({ email: 'test@test.com' })
 				.end(() => {
-					Driver.count().then((newCount) => {
+					Driver.countDocuments().then((newCount) => {
 						assert(count + 1 === newCount);
 						done();
 					});
