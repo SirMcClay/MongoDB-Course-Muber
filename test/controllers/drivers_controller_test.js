10f1283f8ca2b64eba23a2373f604f922e7e380d
sirmcclay <mcclay.design@gmail.com>
2021-02-18 12:15:53 -0300

    create post to /api/drivers to test creation - initial logic

--- test/controllers/drivers_controller_test.js
+++ test/controllers/drivers_controller_test.js
@@ -1,7 +1,14 @@
-const assert = require('asssert');
+const assert = require('assert');
 const request = require('supertest');
 const app = require('../../app');
 
 describe('Drivers controller', (done) => {
-	it('Post to /api/drivers creates a new driver', (done) => {});
+	it('Post to /api/drivers creates a new driver', (done) => {
+		request(app)
+			.post('/api/drivers')
+			.send({ email: 'test@test.com' })
+			.end(() => {
+				done();
+			});
+	});
 });
