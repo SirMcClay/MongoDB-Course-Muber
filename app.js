dee67ed0c20eec4bc42cb023ba429708209f2c1a
sirmcclay <mcclay.design@gmail.com>
2021-02-19 10:02:57 -0300

    some mongo connection additional options configurations

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const routes = require('./routes/routes');
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/muber');
+const options = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+};
+mongoose.connect('mongodb://localhost/muber', options);
 
 app.use(bodyParser.json());
 routes(app);
